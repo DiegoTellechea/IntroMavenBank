0316b392f143f3e31649bb510e833461c6b15413
Diego <tellechea.diego@gmail.com>
2023-10-09 20:53:00 -0300

    IntroMavenBank

---
+++ src/main/java/com/mycompany/intromavenbank/IntroMavenBank.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.intromavenbank;
+
+/**
+ *
+ * @author Diego T. 
+ */
+public class IntroMavenBank {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
